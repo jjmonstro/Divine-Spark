d2d6203b06678703b961b47ad897e90f981a708c
jjmonstro <jpcorreia1211@gmail.com>
2024-10-26 17:13:47 -0300

    update no banco denovo

--- GABARITO_Server/script.sql
+++ GABARITO_Server/script.sql
@@ -13,6 +13,12 @@ drop table Arma
 drop table Bau
 Drop table Sala
 
+ALTER TABLE Pocao
+ADD imagem VARCHAR(50);
+
+-- Alterar a coluna "possui" do tipo BIT para INT
+ALTER TABLE Arma
+ALTER COLUMN possui INT;
 
 DELETE FROM Arma
 DELETE FROM Bau
@@ -44,7 +50,8 @@ CREATE TABLE Pocao (
     nome VARCHAR (50),
     ganho_vida INT,
     ganho_nivel INT,
-    possui BIT
+    possui INT,
+	imagem VARCHAR(50)
 );
 
 CREATE TABLE Bau (
@@ -110,23 +117,27 @@ CREATE TABLE Personagem (
 
 -------------- inserts, são coisas que a api vai só puxar do banco
 INSERT INTO Arma (ID,nome,dano,descricao,possui,imagem) VALUES (1, 'Espada de Madeira', 1.05, 'Uma simples espada de madeira...', 1, 'espadamadeira.png')
-INSERT INTO Arma (ID,nome,dano,descricao,possui,imagem) VALUES (2, 'Espada de Prata', 1.09, 'Uma simples espada de Ferro...', null, 'espadaferro.png')
-INSERT INTO Arma (ID,nome,dano,descricao,possui,imagem) VALUES (3, 'Espada de Ouro', 1.12, 'Uma bela espada de Ouro', null, 'espadaouro.png')
-INSERT INTO Arma (ID,nome,dano,descricao,possui,imagem) VALUES (4, 'Espada de Rubi', 1.2, 'Uma espada forjada inteiramente do rubi mais brilhante', null, 'espadarubi.png')
+INSERT INTO Arma (ID,nome,dano,descricao,possui,imagem) VALUES (2, 'Espada de Prata', 1.2, 'Uma simples espada de Ferro...', null, 'espadaferro.png')
+INSERT INTO Arma (ID,nome,dano,descricao,possui,imagem) VALUES (3, 'Espada de Ouro', 1.4, 'Uma bela espada de Ouro', null, 'espadaouro.png')
+INSERT INTO Arma (ID,nome,dano,descricao,possui,imagem) VALUES (4, 'Espada de Rubi', 1.8, 'Uma espada forjada inteiramente do rubi mais brilhante', null, 'espadarubi.png')
 INSERT INTO Arma (ID,nome,dano,descricao,possui,imagem) VALUES (5, 'Tridente de Netuno', 5, 'O tridente de Netuno, majestoso e imponente, brilha com uma aura celestial. Forjado nos abismos do oceano, suas três pontas cortam as ondas e dominam o reino das profundezas, refletindo o poder supremo do deus dos mares.', null, 'tridentenetuno.png')
 INSERT INTO Arma (ID,nome,dano,descricao,possui,imagem) VALUES (6, 'Trovão de Júpiter', 99, 'O raio de Júpiter, forjado no coração dos céus, relampeja com uma fúria celestial. Este dardo de pura energia divina, envolto em chamas e eletricidade, descerra os céus e domina os trovões com um estrondo primordial. Seu brilho é o selo do poder supremo e a marca da soberania do rei dos deuses.', null, 'trovao.png')
+INSERT INTO Arma (ID,nome,dano,descricao,possui,imagem) VALUES (7, 'Arco Simples', 1.2, 'Um simples arco de madeira', null, 'arcosimples.png')
+INSERT INTO Arma (ID,nome,dano,descricao,possui,imagem) VALUES (8, 'Arco de Caçador', 1.4, 'Um arco especialmente feito para caça', null, 'arcocacador.png')
+INSERT INTO Arma (ID,nome,dano,descricao,possui,imagem) VALUES (9, 'Arco Encantado', 2, 'Um arco misterioso com propriedades mágicas, seus tiros se dividem em dois ao serem lançados', null, 'arcoencantado.png')
+INSERT INTO Arma (ID,nome,dano,descricao,possui,imagem) VALUES (10, 'Cajado da Floresta', 1.4, 'Um cajado de madeira, encantado pelos espiritos da floresta', null, 'cajadofloresta.png')
+INSERT INTO Arma (ID,nome,dano,descricao,possui,imagem) VALUES (11, 'Cajado dos Ventos', 1.7, 'Um reluzendte cajado de prata, infundido com os ventos dos quatro cantos do mundo', null, 'cajadoventos.png')
+INSERT INTO Arma (ID,nome,dano,descricao,possui,imagem) VALUES (12, 'Cajado do Vulcão', 2.3, 'Um cajado de obsidiana, encantado nas profundezas do maior dos vulcões, capaz de destruir um vilarejo com apenas uma de suas bolas de fogo', null, 'cajadovulcao.png')
 
---esses inserts de monstro serão decididos pelo caderno da marina
 
-
-INSERT INTO Pocao (ID,nome,ganho_vida,ganho_nivel) VALUES (1, 'Poção de vida pequena', 10, 0)
-INSERT INTO Pocao (ID,nome,ganho_vida,ganho_nivel) VALUES (2, 'Poção de vida média', 30, 0)
-INSERT INTO Pocao (ID,nome,ganho_vida,ganho_nivel) VALUES (3, 'Poção de vida grande', 50, 0)
-INSERT INTO Pocao (ID,nome,ganho_vida,ganho_nivel) VALUES (4, 'Poção de nivel pequena', 0, 2)
-INSERT INTO Pocao (ID,nome,ganho_vida,ganho_nivel) VALUES (5, 'Poção de nivel média', 0, 4)
-INSERT INTO Pocao (ID,nome,ganho_vida,ganho_nivel) VALUES (6, 'Poção de nivel grande', 0, 8)
-INSERT INTO Pocao (ID,nome,ganho_vida,ganho_nivel) VALUES (7, 'Poção mista', 30, 3)
-INSERT INTO Pocao (ID,nome,ganho_vida,ganho_nivel) VALUES (8, 'Lágrima de Plutão', 999, 999)
+INSERT INTO Pocao (ID,nome,ganho_vida,ganho_nivel,imagem,possui) VALUES (1, 'Poção de vida pequena', 10, 0, 'pocaovpequena.png', null)
+INSERT INTO Pocao (ID,nome,ganho_vida,ganho_nivel,imagem,possui) VALUES (2, 'Poção de vida média', 30, 0, 'pocaovmedia.png', null)
+INSERT INTO Pocao (ID,nome,ganho_vida,ganho_nivel,imagem,possui) VALUES (3, 'Poção de vida grande', 50, 0, 'pocaovgrande.png', null)
+INSERT INTO Pocao (ID,nome,ganho_vida,ganho_nivel,imagem,possui) VALUES (4, 'Poção de nivel pequena', 0, 2, 'pocaonpequena.png', null)
+INSERT INTO Pocao (ID,nome,ganho_vida,ganho_nivel,imagem,possui) VALUES (5, 'Poção de nivel média', 0, 4, 'pocaonmedia.png', null)
+INSERT INTO Pocao (ID,nome,ganho_vida,ganho_nivel,imagem,possui) VALUES (6, 'Poção de nivel grande', 0, 8, 'pocaongrande.png', null)
+INSERT INTO Pocao (ID,nome,ganho_vida,ganho_nivel,imagem,possui) VALUES (7, 'Poção mista', 30, 3, 'pocaom.png', null)
+INSERT INTO Pocao (ID,nome,ganho_vida,ganho_nivel,imagem,possui) VALUES (8, 'Lágrima de Plutão', 999, 999, 'lagrimaplutao.png', null)
 
 INSERT INTO Bau (ID,pocao_ID,arma_ID) VALUES (1, 1, null)
 INSERT INTO Bau (ID,pocao_ID,arma_ID) VALUES (2, 2, null)
@@ -143,8 +154,19 @@ INSERT INTO Bau (ID,pocao_ID,arma_ID) VALUES (12, null, 3)
 INSERT INTO Bau (ID,pocao_ID,arma_ID) VALUES (13, null, 4)
 INSERT INTO Bau (ID,pocao_ID,arma_ID) VALUES (14, null, 5)
 INSERT INTO Bau (ID,pocao_ID,arma_ID) VALUES (15, null, 6)
-INSERT INTO Bau (ID,pocao_ID,arma_ID) VALUES (16, null, 6)
+INSERT INTO Bau (ID,pocao_ID,arma_ID) VALUES (16, null, 7)
+INSERT INTO Bau (ID,pocao_ID,arma_ID) VALUES (17, null, 8)
+INSERT INTO Bau (ID,pocao_ID,arma_ID) VALUES (18, null, 9)
+INSERT INTO Bau (ID,pocao_ID,arma_ID) VALUES (19, null, 10)
+INSERT INTO Bau (ID,pocao_ID,arma_ID) VALUES (20, null, 11)
+INSERT INTO Bau (ID,pocao_ID,arma_ID) VALUES (21, null, 12)
+
+
+delete from Sala
 delete from Bau
+delete from Pocao
+
+
 select * from Bau
 select * from Arma
 select * from Pocao
@@ -193,13 +215,9 @@ INSERT INTO Sala (ID,monstro_ID,bau_id,esquerda,direita,frente,tras,imagem) VALU
 INSERT INTO Sala (ID,monstro_ID,bau_id,esquerda,direita,frente,tras,imagem) VALUES (16, 1, null, null, null, null, 5, 'salan.png')
 INSERT INTO Sala (ID,monstro_ID,bau_id,esquerda,direita,frente,tras,imagem) VALUES (17, 12, 0, null, null, null, 13, 'salan.png')
 
-INSERT INTO Personagem (ID,nome,vidaMax,vidaAtual,forca,agilidade,nivel,equipamento,imagem) VALUES (1, 'Eneias', 10, 10, 10, 10, 10, 1, 'p1.png')
-INSERT INTO Personagem (ID,nome,vidaMax,vidaAtual,forca,agilidade,nivel,equipamento,imagem) VALUES (2, 'Réia', 10, 10, 10, 10, 10, 1, 'p2.png')
-INSERT INTO Personagem (ID,nome,vidaMax,vidaAtual,forca,agilidade,nivel,equipamento,imagem) VALUES (3, 'Jasão', 10, 10, 10, 10, 10, 1, 'p3.png')
+INSERT INTO Personagem (ID,nome,vidaMax,vidaAtual,forca,agilidade,nivel,equipamento,imagem) VALUES (1, 'Lorax', 10, 10, 10, 10, 10, 1, 'p1.png')
+INSERT INTO Personagem (ID,nome,vidaMax,vidaAtual,forca,agilidade,nivel,equipamento,imagem) VALUES (2, 'Lorax2', 10, 10, 10, 10, 10, 1, 'p2.png')
+INSERT INTO Personagem (ID,nome,vidaMax,vidaAtual,forca,agilidade,nivel,equipamento,imagem) VALUES (3, 'Lorax3', 10, 10, 10, 10, 10, 1, 'p3.png')
 
---INSERT INTO Personagem(ID, nome, forca,agilidade, nivel, vidaAtual, vidaMax, imagem) VALUES(1, 'Eneias', 5, 2, 1, 3, 3, 'p1.png')
---INSERT INTO Personagem(ID, nome, forca,agilidade, nivel, vidaAtual, vidaMax, imagem) VALUES(2, 'Réia', 2, 3, 1, 5, 5, 'p2.png')
---INSERT INTO Personagem(ID, nome, forca,agilidade, nivel, vidaAtual, vidaMax, imagem) VALUES(3, 'Jasão', 3,5, 1, 2, 2, 'p3.png')
---o pessoal colocou esses commits de forma errada, colocaram qunatos pontos ele tem no status ao invés de quanto do status ele tem	
 select * from Personagem
 drop table Personagem
